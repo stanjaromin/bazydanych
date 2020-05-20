7f04d39bd54df7f0411f96739171ca195250ef3f
Stanislaw Jaromin <jaromin@onet.pl>
2020-05-20 21:40:17 +0200

    Playing with db

--- contacts.py
+++ contacts.py
@@ -5,4 +5,13 @@ db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email
 db.execute("INSERT INTO contacts(name, phone, email) VALUES('Tim', 6565678, 'time@malpa.com')")
 db.execute("INSERT INTO contacts VALUES ('Brian', 1234, 'brian@malpa.pl')")
 
+cursor = db.cursor()
+cursor.execute("SELECT * FROM contacts")
+
+print(cursor.fetchall())
+
+for row in cursor:
+    print(row)
+
+cursor.close()
 db.close()
