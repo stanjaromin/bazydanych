2e2933440261eda7163a122c474445aa420313e7
Stanislaw Jaromin <jaromin@onet.pl>
2020-05-20 21:33:47 +0200

    Playing with db

--- contacts.py
+++ contacts.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
-db.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
 db.execute("INSERT INTO contacts(name, phone, email) VALUES('Tim', 6565678, 'time@malpa.com')")
 db.execute("INSERT INTO contacts VALUES ('Brian', 1234, 'brian@malpa.pl')")
 
